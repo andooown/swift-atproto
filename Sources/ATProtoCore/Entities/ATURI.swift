85223d1da49b469e37685c6d19251816c69d1071
andooown <andooown.dev@gmail.com>
2023-09-14 01:05:03 +0900

    Conform to `Hashable`

--- Sources/ATProtoCore/Entities/ATURI.swift
+++ Sources/ATProtoCore/Entities/ATURI.swift
@@ -4,7 +4,7 @@ import RegexBuilder
 
 // https://github.com/bluesky-social/atproto/blob/0533fab68ea32df4e00948ddfc2422c6f900223a/packages/syntax/src/aturi.ts
 
-public struct ATURI {
+public struct ATURI: Hashable {
     public var host: String
     public var path: String?
     public var query: OrderedDictionary<String, String?>
