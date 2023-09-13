ba68a789f378e3b3ee47f443cd1c64cb55e41593
andooown <andooown.dev@gmail.com>
2023-09-13 22:47:33 +0900

    Revert "[tmp] Failure test case on iOS 17"
    This reverts commit 24b82fb5132cc5ebf975394d7b97eac5f98727d8.

--- Tests/ATProtoXRPCTests/TemporaryTests.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import XCTest
-
-final class TemporaryTests: XCTestCase {
-    func testExample() throws {
-        let data = #""at://did:plc:z72i7hdynmk6r22z27h6tvur/app.bsky.feed.generator/whats-hot""#.data(using: .utf8)!
-        XCTAssertNoThrow(try JSONDecoder().decode(URL.self, from: data))
-    }
-}
