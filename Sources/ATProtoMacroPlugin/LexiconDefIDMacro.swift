d5d0447e5d4385da062487a43f8bc95c3f353050
andooown <andooown.dev@gmail.com>
2023-10-21 02:30:25 +0900

    Implement macro

--- Sources/ATProtoMacroPlugin/LexiconDefIDMacro.swift
+++ Sources/ATProtoMacroPlugin/LexiconDefIDMacro.swift
@@ -1,8 +1,42 @@
-//
-//  File.swift
-//  
-//
-//  Created by Yoshikazu Ando on 2023/10/20.
-//
-
+import ATProtoCore
 import Foundation
+import SwiftSyntax
+import SwiftSyntaxMacros
+
+public enum LexiconDefIDMacro: ExpressionMacro {
+    private enum MacroError: Error, CustomStringConvertible {
+        case noStaticStringLiteral
+        case malformedID(ExprSyntax)
+
+        var description: String {
+            switch self {
+            case .noStaticStringLiteral:
+                return "#LexiconDefID requires a static string literal"
+
+            case .malformedID(let value):
+                return "Malformed ID string: \(value)"
+            }
+        }
+    }
+
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> ExprSyntax {
+        guard let argument = node.argumentList.first?.expression,
+              let segments = argument.as(StringLiteralExprSyntax.self)?.segments,
+              segments.count == 1,
+              case .stringSegment(let literalSegment)? = segments.first
+        else {
+            throw MacroError.noStaticStringLiteral
+        }
+
+        do {
+            _ = try LexiconDefinitionID(string: literalSegment.content.text)
+        } catch {
+            throw MacroError.malformedID(argument)
+        }
+
+        return "try! LexiconDefinitionID(string: \(argument))"
+    }
+}
--- Tests/ATProtoMacroPluginTests/LexiconDefIDMacroTests.swift
+++ Tests/ATProtoMacroPluginTests/LexiconDefIDMacroTests.swift
@@ -1,6 +1,51 @@
+import SwiftSyntaxMacros
+import SwiftSyntaxMacrosTestSupport
 import XCTest
 
+@testable import ATProtoMacroPlugin
+
 final class LexiconDefIDMacroTests: XCTestCase {
-    func testExample() throws {
+    private let macros = ["LexiconDefID": LexiconDefIDMacro.self]
+
+    func testExpansion() {
+        assertMacroExpansion(
+            """
+            let valid = #LexiconDefID("com.example.foo")
+            """,
+            expandedSource: """
+            let valid = try! LexiconDefinitionID(string: "com.example.foo")
+            """,
+            macros: macros
+        )
+    }
+
+    func testExpansionWithNonStaticString() {
+        assertMacroExpansion(
+            #"""
+            let invalid = #LexiconDefID("com.example.\(name)")
+            """#,
+            expandedSource: #"""
+            let invalid = #LexiconDefID("com.example.\(name)")
+            """#,
+            diagnostics: [
+                DiagnosticSpec(message: #"#LexiconDefID requires a static string literal"#, line: 1, column: 15, severity: .error),
+            ],
+            macros: macros
+        )
+    }
+
+    func testExpansionWithInvalidID() {
+        assertMacroExpansion(
+            """
+            let invalid = #LexiconDefID("invalid.")
+            """,
+            expandedSource: """
+            let invalid = #LexiconDefID("invalid.")
+            """,
+            diagnostics: [
+                DiagnosticSpec(message: #"Malformed ID string: "invalid.""#, line: 1, column: 15, severity: .error),
+            ],
+            macros: macros
+        )
     }
 }
