5a59400121901eee367a309dd729313cbf24922a
andooown <andooown.dev@gmail.com>
2023-10-21 03:14:40 +0900

    Bump up lexicon-gen to 0.0.6 and regenerate API

--- Package.swift
+++ Package.swift
@@ -50,6 +50,7 @@ let package = Package(
             dependencies: [
                 "ATProtoXRPC",
                 "ATProtoCore",
+                "ATProtoMacro",
             ]
         ),
         .target(
