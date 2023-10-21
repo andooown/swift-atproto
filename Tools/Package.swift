13d45a18a48dd8162854c2a611d3684b69399a0b
andooown <andooown.dev@gmail.com>
2023-10-22 03:19:55 +0900

    Update lexicon-gen to 0.0.7

--- Tools/Package.swift
+++ Tools/Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .macOS(.v13)
     ],
     dependencies: [
-        .package(url: "https://github.com/andooown/lexicon-gen.git", revision: "0.0.6"),
+        .package(url: "https://github.com/andooown/lexicon-gen.git", revision: "0.0.7"),
     ],
     targets: [
         .target(name: "Tools", path: "")
